dc30249d08f96eabfca13cd47c6d503b2f8c8ec0
Evgeniya Dontsova <jennyakateva@gmail.com>
2023-10-30 15:11:32 -0600

    update filter faces

--- filter_images_with_faces_move.py
+++ filter_images_with_faces_move.py
@@ -2,21 +2,20 @@ import cv2
 import os
 from pathlib import Path
 
-check_dir = Path('./check_faces')
-save_dir = Path('./filter_with_faces')
-face_cascade = cv2.CascadeClassifier('faces.xml')
+INPUT_FOLDER = Path('./check_faces')
+OUTPUT_FOLDER = Path('./filter_with_faces')
+CASCADE = cv2.CascadeClassifier('faces.xml')
 
 images_with_faces = []
 
-for file in check_dir.iterdir():
+for file in INPUT_FOLDER.iterdir():
     image = cv2.imread(str(file))
-    faces = face_cascade.detectMultiScale(image, 1.11, 4)
+    faces = CASCADE.detectMultiScale(image, 1.11, 4)
 
     if len(faces) > 0:
         images_with_faces.append(file)
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 4)
 
-        save_filepath = str(save_dir) + '/' +  file.stem + file.suffix
+        save_filepath = str(OUTPUT_FOLDER) + '/' +  file.stem + file.suffix
         cv2.imwrite(save_filepath, image)
-# %%
