5c57edf893b81cb164112926692505c7acc0aa96
Evgeniya Dontsova <jennyakateva@gmail.com>
2023-10-31 16:11:33 -0600

    add google trabslate

---
+++ google_translate.py
@@ -0,0 +1,9 @@
+#%%
+from googletrans import Translator
+
+translator = Translator()
+translation = translator.translate('Oa mai oe?', dest='en').text
+print(translation)
+
+#some issues with connections
+# %%
